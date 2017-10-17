0d218a35b00759b2ea92f54f90452f07c4acfa48
Nikita Laitinen <nikitalaitinen@ya.ru>
2017-10-17 22:15:49 +0400

    create all dinamic events javascript

--- app/js/app.js
+++ app/js/app.js
@@ -13,9 +13,38 @@
   	cssEase: 'linear',
   	dots: true,
   	customPaging: function(slick,index) {
-        return '<a>'  + '</a>';
+        return '<a>' + '</a>';
     }
   });
 
- 
+  $('.map__button').on('click', function(event) {
+    event.preventDefault();
+    $('.write-us').arcticmodal({
+      afterClose: function() {
+        $('.write-us input, .write-us textarea').val('');
+      }
+    });
+  });
+
 })();
+
+function initMap() {
+  console.log('Google Maps Api was init');
+
+  var coordinates = {lat: 59.9387942, lng: 30.3208946},
+    markerImage = 'imgs/content/map-marker.png',
+    zoom = 17,
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: zoom,
+        disableDefaultUI: true
+    }),
+    marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        icon: markerImage
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
--- gulp/path/path.foundation.js
+++ gulp/path/path.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
 	'./bower_components/jquery/dist/jquery.js',
-	'./bower_components/slick-carousel/slick/slick.js'
+	'./bower_components/slick-carousel/slick/slick.js',
+	'./bower_components/arcticModal/arcticmodal/jquery.arcticmodal.js'
 ];
